d9cf5280f8c7eb1215e405e2fa3c0314a91b771a
Talha <talhaiftikhar12549@gmail.com>
2024-07-26 10:18:13 +0500

    Edit Modal can Now Show validations. fix 1

--- src/components/BodyComponent.vue
+++ src/components/BodyComponent.vue
@@ -134,7 +134,7 @@
                 </div>
               </div>
               <div class="modal-footer">
-                <button type="submit" class="btn btn-primary" @click="editdata" >Save changes
+                <button type="submit" class="btn btn-primary"  >Save changes
                 </button>
               </div>
             </form>
